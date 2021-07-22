8e5862b0f4a8d4c9035d156b84adbd774a329192
mika <mmmmmmika88@gmail.com>
2021-07-22 12:52:32 +0900

    1st-commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::orderBy('created_at', 'desc')->get();
+        return view('index', ['todos' => $todos]);
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Todo::$rules);
+        $todos = $request->all();
+        Todo::create($todos);
+        return redirect('/');
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Todo::$rules);
+        $todo = $request->all();
+        unset($todo['_token']);
+        Todo::where('id', $request->id)->update($todo);
+        return redirect('/');
+    }
+
+    public function delete(Request $request)
+    {
+        $todo = Todo::find($request->id);
+        $todo->delete();
+        return redirect('/');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="container">
+        <h1>Todo List</h1>
+
+        @if (count($errors) > 0)
+            <p>20文字以下で入力してください</p>
+        @endif
+
+        <form action="/todo/create" method="post">
+            @csrf
+            <input type="text" name="content" class="main-form">
+            <button class="add-btn">追加</button>
+        </form>
+        <table>
+            <tr>
+                <th>作成日</th>
+                <th>タスク名</th>
+                <th>更新</th>
+                <th>削除</th>
+            </tr>
+
+            @foreach ($todos as $todo)
+            <tr>
+                <td>{{ $todo->created_at }}</td>
+                <td> <input type="text" name="content" class="sub-form" value="{{ $todo->content }}"></td>
+
+                <td>
+                    <form action="/todo/update" method="GET">
+                        @csrf
+                        <input type="hidden" name="content" value="{{$todo->id}}">
+                        <button type="submit" class="update-btn">更新</button>
+
+                    </form>
+
+                <td>
+                    <form action="/todo/delete" method="DELETE">
+                        @method('DELETE')
+                        @csrf
+                        <input type="hidden" name="id" value="{{$todo->id}}">
+                        <button type="submit" class="delete-btn">削除</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
